9cca642626802bd5e5e45f5f1fe9d8d76b647fc1
badari07 <badari0793@gmail.com>
2020-04-17 00:26:05 +0530

    dashboard update

--- sections/dashboard/src/pages/dashboard.js
+++ sections/dashboard/src/pages/dashboard.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Router } from "@reach/router";
 import { navigate } from "gatsby";
+import IdentityModal from "react-netlify-identity-widget";
 import Layout from "../components/layout";
 import Profile from "../components/profile";
 import RouteBase from "../components/route-base";
 import RouteSecret from "../components/route-secret";
 import RouteLogin from "../components/route-login";
+import "react-netlify-identity-widget/styles.css";
 
 const Dashboard = ({ location }) => {
-  // const [isVisible, setVisibility] = useState(false);
-  // const showModal = () => setVisibility(true);
+  const [isVisible, setVisibility] = useState(false);
+  const showModal = () => setVisibility(true);
 
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
@@ -23,8 +25,12 @@ const Dashboard = ({ location }) => {
       <Router>
         <RouteBase path="/dashboard/base" />
         <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
       </Router>
+      <IdentityModal
+        showDialog={isVisible}
+        onCloseDialog={() => setVisibility(false)}
+      />
     </Layout>
   );
 };
