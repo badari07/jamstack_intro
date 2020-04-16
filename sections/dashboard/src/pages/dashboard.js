18cf18e3aa631538a4125adcadd091f9fac3034b
badari07 <badari0793@gmail.com>
2020-04-16 21:37:45 +0530

    adding dashboard

---
+++ sections/dashboard/src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../components/layout";
+
+const Dashbboard = () => {
+  return (
+    <Layout>
+      <p>TODO:CREATE A DASHBOARD</p>
+    </Layout>
+  );
+};
+
+export default Dashbboard;
