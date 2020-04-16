1676551175bddc4714445e836f8529b694988ed3
badari07 <badari0793@gmail.com>
2020-04-16 23:14:37 +0530

    adding routes

--- sections/dashboard/src/pages/dashboard.js
+++ sections/dashboard/src/pages/dashboard.js
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { Router } from "@reach/router";
+import { navigate } from "gatsby";
 import Layout from "../components/layout";
+import Profile from "../components/profile";
+import RouteBase from "../components/route-base";
+import RouteSecret from "../components/route-secret";
+import RouteLogin from "../components/route-login";
 
-const Dashbboard = () => {
+const Dashboard = ({ location }) => {
+  // const [isVisible, setVisibility] = useState(false);
+  // const showModal = () => setVisibility(true);
+
+  useEffect(() => {
+    if (location.pathname.match(/^\/dashboard\/?$/)) {
+      navigate("/dashboard/login", { replace: true });
+    }
+  }, []);
+
   return (
     <Layout>
-      <p>TODO:CREATE A DASHBOARD</p>
+      <Profile />
+      <Router>
+        <RouteBase path="/dashboard/base" />
+        <RouteSecret path="/dashboard/secret" />
+        <RouteLogin path="/dashboard/login" />
+      </Router>
     </Layout>
   );
 };
 
-export default Dashbboard;
+export default Dashboard;
