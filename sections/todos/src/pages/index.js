ec9861f44061f17f115c38625be1b3412776c1bf
badari07 <badari0793@gmail.com>
2020-04-18 23:23:38 +0530

    feat:setup creat todo and form to submit

--- sections/todos/src/pages/index.js
+++ sections/todos/src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./index.module.css";
 import Todo from "../components/todo";
+import Form from "../components/form";
 
 export default () => {
   const [status, setStatus] = useState("loading");
@@ -26,9 +27,12 @@ export default () => {
       canceled = true;
     };
   }, [status]);
+
+  const reloadTodos = () => setStatus("loading");
   return (
     <main>
       <h1 className={styles.heading}>JAMstack Todos</h1>
+      <Form reloadTodos={reloadTodos} />
       {todos ? (
         <ul className={styles.todos}>
           {todos.map((todo) => (
